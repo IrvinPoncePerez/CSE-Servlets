41eac48e94473d4bbf03ef9c110e3098c5b4c8d7
Irvin Ponce Perez <IrvinPoncePerez@gmail.com>
2015-07-28 18:14:12 -0500

    28-JUL-2015
    Method POST CalvarioServlets/PostPictureServlet

---
+++ src/java/Servlets/PostPictureServlet.java
@@ -0,0 +1,140 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlets;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringReader;
+import java.sql.Blob;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.rowset.serial.SerialBlob;
+import sun.misc.BASE64Decoder;
+
+/**
+ *
+ * @author IRVIN
+ */
+@WebServlet(name = "PostPictureServlet", urlPatterns = {"/PostPictureServlet"})
+public class PostPictureServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet PostPictureServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet PostPictureServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        StringBuilder builder = new StringBuilder();
+        BufferedReader reader = request.getReader();
+        Blob blob;
+        String employeeNumber;
+        
+        try{
+            String line;
+            while ((line = reader.readLine()) != null){
+                builder.append(line);
+            }
+                
+            JsonReader jsonReader = Json.createReader(new StringReader(builder.toString()));
+            JsonObject jsonObject = jsonReader.readObject();
+            jsonReader.close();
+            
+            employeeNumber = jsonObject.getString("employee_number");
+            blob = decodeToImage(jsonObject.getString("picture"));
+            
+            response.setStatus(HttpServletResponse.SC_OK);
+        } catch (IOException ex){
+            System.out.println(ex.getMessage());
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        } finally {
+            reader.close();
+        }      
+        
+    }
+    
+    public SerialBlob decodeToImage(String stringImage){
+        try {
+            BASE64Decoder decoder = new BASE64Decoder();
+            byte[] imageByte = decoder.decodeBuffer(stringImage);
+            blob = new SerialBlob(imageByte);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        
+        return blob;
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
+
